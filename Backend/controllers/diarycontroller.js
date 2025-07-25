f5467906e51aeddc0ab3c8f3e299fdcfbe3dc727
cb671 <corymb33@gmail.com>
2025-07-25 10:36:35 +0100

    controller,model and router added

---
+++ Backend/controllers/diarycontroller.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    index,
+    getTop,
+    show,
+    create,
+    destroy,
+    update
+};
---
+++ Backend/routers/diaryrouter.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+
+const diaryController = require('../controllers/diarycontroller');
+
+const diaryRouter = Router();
+
+diaryRouter.get("/", diaryController.index);
+diaryRouter.get("/top", diaryController.getTop);
+diaryRouter.get("/:id", diaryController.show);
+diaryRouter.post("/", diaryController.create);
+diaryRouter.delete("/:id", diaryController.destroy);
+diaryRouter.patch("/:id", diaryController.update);
+
+module.exports = diaryRouter;
