9906931c32feacb69b0e5eee9969574bd0542ac4
cb671 <corymb33@gmail.com>
2025-07-25 11:44:46 +0100

    added cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const dairyRouter = require('./Backend/routers/diaryrouter');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).json({
